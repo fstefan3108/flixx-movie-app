278607cf17b967b7ed186fd9b7e209d86a09c7be
fstefan3108 <e14stefanfilipovic@gmail.com>
2025-05-13 18:45:08 +0200

    aflsdofksdof

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ function getNormalizedPath() {
     return path === "/" ? "" : path;
 }
 
+
 const globalPath = {
 
     currentPage: getNormalizedPath(),
@@ -36,7 +37,7 @@ export default globalPath;
 // Active Links //
 
 
-
+console.log("Current page:", globalPath.currentPage);
 
 function activeLink() {
 
